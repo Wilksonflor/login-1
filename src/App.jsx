3ca67845c306f277d0daa563dc99d80b28adb451
Wilksonflor <wilksonflor12@gmail.com>
2024-03-02 16:00:37 -0300

    Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import Menu from "./components/Menu/Menu";
-import { Rotas } from "./components/Rotas/Rotas";
+// import { Rotas } from "./components/Rotas/Rotas";
 // import { CriarConta } from "./components/criarConta/CriarConta";
 
 export default function App() {
   return (
     <>
-      <Rotas />
+      {/* <Rotas /> */}
       <Menu />
     </>
   );
